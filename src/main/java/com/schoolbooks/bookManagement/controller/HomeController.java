c5f51f046fe8a6d2de006e8ae89509f27c0821bd
PraneethMalshan <praneethmalshan12345@gmail.com>
2025-05-07 12:10:37 +0530

    Start to Create project

---
+++ src/main/java/com/schoolbooks/bookManagement/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.schoolbooks.bookManagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String home(){
+        return "Welcome to School Book Management System!";
+    }
+}
