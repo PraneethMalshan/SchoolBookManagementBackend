2edb44a5f787664273e1701235d796cffedc8799
PraneethMalshan <praneethmalshan12345@gmail.com>
2025-05-25 21:26:20 +0530

    init project

---
+++ src/main/java/com/kdpm/schoolTextbookManagement/SchoolTextbookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kdpm.schoolTextbookManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTextbookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTextbookManagementApplication.class, args);
+	}
+
+}
